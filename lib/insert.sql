8014a0dbfb2be92fddbd34266698a9e03f705233
Justin Hall <hall.justin.d@gmail.com>
2019-04-07 21:51:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES ("Rowdy", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Rowdy", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES ("Tabitha", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Tabitha", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES ("Sergeant Brown", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Sergeant Brown", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES ("Melissa", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Melissa", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES ("Grinch", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Grinch", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES ("Wendy", 12, "Male", "Brown", "Smooth", 1)
+INSERT INTO bears VALUES ("Wendy", 12, "Male", "Brown", "Smooth", 1);
 
-INSERT INTO bears VALUES (null, 28, "Male", "Brown", "Hungry", 0)
+INSERT INTO bears VALUES (null, 28, "Male", "Brown", "Hungry", 0);
