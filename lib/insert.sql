4b8bdcafd5510e293310c7c7c31f470f3fef1249
Justin Hall <hall.justin.d@gmail.com>
2019-04-07 21:52:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Smooth", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Smooth", 1);
 
 INSERT INTO bears VALUES ("Rowdy", 12, "Male", "Brown", "Smooth", 1);
 
