18494114849cc5f47019686413c59e1d93d4c714
Justin Hall <hall.justin.d@gmail.com>
2019-04-07 21:50:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Rowdy", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Tabitha", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Sergeant Brown", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Melissa", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Grinch", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "Male", "Brown", "Smooth", true)
+INSERT INTO bears VALUES ("Wendy", 12, "Male", "Brown", "Smooth", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, "Male", "Brown", "Hungry", false)
+INSERT INTO bears VALUES (NULL, 28, "Male", "Brown", "Hungry", 0)
